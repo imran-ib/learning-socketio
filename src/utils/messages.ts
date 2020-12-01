8a2c7b0c18b1afa9f5f130e60c27dc35520b7ccf
Imran Irshad <imran.ib@live.com>
2020-12-01 20:08:40 +0300

    docs(main-scope): :zap: added coments
    added two new comments to test new auto generate log

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,9 +1,11 @@
+//NOTE: this function is responsible for generating message for user with date
 export const generateMessage = (text: string) => {
   return {
     text,
     createdAt: new Date().getTime(),
   };
 };
+//NOTE: this function is responsible for generating location message with date
 export const generateLocationMessage = (url: string) => {
   return {
     url,
